2238ce2156d320410c9ba2be42e315bff108aff3
Namratha <vatti.m@husky.neu.edu>
2021-02-07 18:39:11 -0500

    third song

---
+++ Pages/Song3.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace lab2.Pages
+{
+    public class Song3Model : PageModel
+    {
+        private readonly ILogger<Song3Model> _logger;
+
+        public Song3Model(ILogger<Song3Model> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
