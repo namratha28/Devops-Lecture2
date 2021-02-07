67c581a985168833f8941caf1b235414aebe7cfc
Namratha <vatti.m@husky.neu.edu>
2021-02-07 16:28:42 -0500

    First song

---
+++ Pages/Song1.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace lab2.Pages
+{
+    public class Song1Model : PageModel
+    {
+        private readonly ILogger<Song1Model> _logger;
+
+        public Song1Model(ILogger<Song1Model> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
