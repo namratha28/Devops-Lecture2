cc3ce9038dcaf264fe021f964a3805a113385d9d
Namratha <vatti.m@husky.neu.edu>
2021-02-07 18:33:06 -0500

    second song

---
+++ Pages/Song2.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace lab2.Pages
+{
+    public class Song2Model : PageModel
+    {
+        private readonly ILogger<Song2Model> _logger;
+
+        public Song2Model(ILogger<Song2Model> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
